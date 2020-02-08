421ba8b168c1d51973d3fe996e176cd04ccf385b
fatcatdog <duchenjacob@gmail.com>
2020-02-08 14:46:09 -0500

    404 page is broken

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -5,10 +5,23 @@ import About from './components/About';
 import Contact from './components/Contact';
 import Blogs from './components/Blogs';
 import Blog from './components/Blog';
-import NoMatchPage from './components/NoMatchPage';
-
+// import NoMatchPage from './components/NoMatchPage';
+import Header from './components/Header';
 import * as serviceWorker from './serviceWorker';
 import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
+import './styles/NoMatchPage.css';
+
+function NoMatchPage() {
+  return(
+    <div>
+      <div className="everything">
+        <Header />
+          <h2>404</h2>
+        <p>Something went very wrong :(</p>
+      </div>
+    </div>
+  )
+}
 
 const routing = (
   <Router>
