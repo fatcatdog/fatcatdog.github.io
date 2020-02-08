c6c3ab10f7146d5980edaac79ab491231aad2734
fatcatdog <duchenjacob@gmail.com>
2020-02-08 14:40:06 -0500

    errorpage edit

--- my-app/src/components/NoMatchPage.js
+++ my-app/src/components/NoMatchPage.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Header from './Header';
+import '../styles/NoMatchPage.css';
 
 function NoMatchPage() {
   return(
     <div>
-      <Header />
-      <h2>NoMatchPage</h2>
+      <div className="everything">
+        <Header />
+          <h2>404</h2>
+        <p>Something went very wrong :(</p>
+      </div>
     </div>
   )
 }
